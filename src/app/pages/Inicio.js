803cd488f0e460e25e7da327f86901481c6eed49
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-11-13 00:39:57 -0500

    feat: mejora en componente countUp

--- src/app/pages/Inicio.js
+++ src/app/pages/Inicio.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import CountUp from "react-countup";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import styled from "styled-components";
@@ -109,25 +109,25 @@ function CountingBoxComponent({
   label = "",
   background,
   color = "inherit",
+  active = false,
 }) {
   return (
     <CountingBox background={background} color={color}>
-      <CountUp className="counter" end={end} duration="1" />
+      {!active ? (
+        <span className="counter">0</span>
+      ) : (
+        <CountUp className="counter" start={0} end={end} duration={1} />
+      )}
       {label && <span className="label">{label}</span>}
     </CountingBox>
   );
 }
 
-const images = [
-  { image: carousel_1, legend: "" },
-  { image: carousel_2, legend: "" },
-  { image: carousel_3, legend: "" },
-  { image: carousel_4, legend: "" },
-];
-
 function Inicio() {
   const { configTheme, resetTheme } = useThemeHeader();
   const scrollPosition = useScrollPosition();
+  const refContainerCountingBox = useRef();
+  const [countUpActive, setCountUpActive] = useState(false);
 
   const initTheme = useCallback(() => {
     configTheme("transparent", null, null, false);
@@ -144,6 +144,27 @@ function Inicio() {
       initTheme();
     }
   }, [initTheme, resetTheme, scrollPosition]);
+
+  useEffect(() => {
+    const scrollTop = scrollPosition;
+    const scrollBottom = scrollPosition + window.innerHeight;
+    const {
+      offsetTop: scrollRefTop,
+      clientHeight,
+      scrollRefBottom = scrollRefTop + clientHeight,
+    } = refContainerCountingBox.current;
+
+    setCountUpActive(
+      scrollBottom >= scrollRefTop && scrollRefBottom >= scrollTop
+    );
+  }, [scrollPosition]);
+
+  const images = [
+    { image: carousel_1, legend: "" },
+    { image: carousel_2, legend: "" },
+    { image: carousel_3, legend: "" },
+    { image: carousel_4, legend: "" },
+  ];
 
   const renderStaticContent = () => {
     return (
@@ -195,57 +216,32 @@ function Inicio() {
         corporis error natus blanditiis molestiae sapiente alias at accusamus
         cupiditate nemo perspiciatis quasi est repellendus accusantium modi
         praesentium, minus non ut officia obcaecati sint perferendis. Odit quia
-        blanditiis enim deleniti praesentium repellendus dolor delectus fugit
-        quibusdam molestias doloremque voluptates error, natus accusamus,
-        voluptatum nostrum quasi. Impedit provident quos nihil perspiciatis
-        itaque illum. Quibusdam enim ullam itaque nisi officia voluptate fugiat
-        recusandae sunt reprehenderit assumenda! Obcaecati nesciunt rem
-        cupiditate. Inventore, similique iure optio quas numquam, facere id at
-        laborum delectus in dolorum nisi molestiae enim quisquam suscipit.
-        Repellat, itaque? Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Eum doloremque iste dolores corporis error natus blanditiis
-        molestiae sapiente alias at accusamus cupiditate nemo perspiciatis quasi
-        est repellendus accusantium modi praesentium, minus non ut officia
-        obcaecati sint perferendis. Odit quia blanditiis enim deleniti
-        praesentium repellendus dolor delectus fugit quibusdam molestias
-        doloremque voluptates error, natus accusamus, voluptatum nostrum quasi.
-        Impedit provident quos nihil perspiciatis itaque illum. Quibusdam enim
-        ullam itaque nisi officia voluptate fugiat recusandae sunt reprehenderit
-        assumenda! Obcaecati nesciunt rem cupiditate. Inventore, similique iure
-        optio quas numquam, facere id at laborum delectus in dolorum nisi
-        molestiae enim quisquam suscipit. Repellat, itaque? Lorem ipsum dolor
-        sit amet consectetur, adipisicing elit. Eum doloremque iste dolores
-        corporis error natus blanditiis molestiae sapiente alias at accusamus
-        cupiditate nemo perspiciatis quasi est repellendus accusantium modi
-        praesentium, minus non ut officia obcaecati sint perferendis. Odit quia
-        blanditiis enim deleniti praesentium repellendus dolor delectus fugit
-        quibusdam molestias doloremque voluptates error, natus accusamus,
-        voluptatum nostrum quasi. Impedit provident quos nihil perspiciatis
-        itaque illum. Quibusdam enim ullam itaque nisi officia voluptate fugiat
-        recusandae sunt reprehenderit assumenda! Obcaecati nesciunt rem
-        cupiditate. Inventore, similique iure optio quas numquam, facere id at
-        laborum delectus in dolorum nisi molestiae enim quisquam suscipit.
         Repellat, itaque?
       </p>
-      <ContainerCountingBox>
+      <ContainerCountingBox ref={refContainerCountingBox}>
         <CountingBoxComponent
+          active={countUpActive}
           background="var(--color-sub-primary)"
           color="var(--color-white)"
           end={10}
           label="Publicaciones"
         />
         <CountingBoxComponent
+          active={countUpActive}
           background="var(--color-orange)"
           end={8}
           label="Investigaciones"
         />
         <CountingBoxComponent
+          active={countUpActive}
           background="var(--color-red)"
           end={34}
           label="Lectores"
         />
       </ContainerCountingBox>
-      <SectionBasic title="Últimas Publicaciones"></SectionBasic>
+      <SectionBasic title="Últimas Publicaciones">
+        
+      </SectionBasic>
     </SectionWithCarousel>
   );
 }
