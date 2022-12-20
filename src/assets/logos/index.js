95d70cefb7877322dcf2715f90e9dad14fd83767
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-12-20 15:29:25 -0500

    feat: spring 4

--- src/assets/logos/index.js
+++ src/assets/logos/index.js
@@ -1,5 +1,4 @@
-import logo from "./logo.png";
 import logoEscudoText from "./escudoText.svg";
 import logoEscudo from "./logoEscudo.png";
 import logoHome from "./logoHome.svg";
-export { logo, logoEscudoText, logoEscudo, logoHome };
+export { logoEscudoText, logoEscudo, logoHome };
